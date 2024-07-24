3faa60c53e45dc91bce41a2ebcfe3128a77f10ce
uzair-shakeel <uzairshakeel00000@gmail.com>
2024-07-24 11:08:36 +0500

    botttom Drawer Fixed

--- client/src/components/bottomDrawer.jsx
+++ client/src/components/bottomDrawer.jsx
@@ -1,6 +1,7 @@
 // SideBar.js
 import React from "react";
 import {
+  Box,
   Drawer,
   DrawerBody,
   DrawerHeader,
@@ -9,6 +10,7 @@ import {
   DrawerCloseButton,
 } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
+import { RxCross2 } from "react-icons/rx";
 
 function BottomDrawer({ isOpen, onClose, finalFocusRef, isSignedIn }) {
   return (
@@ -19,13 +21,13 @@ function BottomDrawer({ isOpen, onClose, finalFocusRef, isSignedIn }) {
       finalFocusRef={finalFocusRef}
     >
       <DrawerOverlay />
-      <DrawerContent>
-        <DrawerHeader>
-          <DrawerCloseButton />
-        </DrawerHeader>
+      <DrawerContent className="relative" bg={"transparent"}>
+        <Box className="absolute bg-white  -top-12 -translate-x-1/2 left-1/2 flex items-center justify-center p-2 rounded-full shadow-lg">
+          <RxCross2 size={22} onClick={onClose} />
+        </Box>
 
-        <DrawerBody>
-          <ul className="flex flex-col gap-9 font-[700] text-[23px] mb-[300px]">
+        <DrawerBody bg={"white"}>
+          <ul className="flex flex-col  gap-9 font-[500] text-[20px] mb-[200px]">
             <Link onClick={onClose} to="/" className="hover:text-black/70">
               <li>Home</li>
             </Link>
