d2271d4507f1ebd8d0f88bfc98d302ca09a6ada0
sn1tch1 <65768436+sn1tch1@users.noreply.github.com>
2024-08-21 15:30:04 +0300

    Update Carousel.jsx

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -2,14 +2,23 @@ import React from "react";
 import Image1 from "../assets/home/carousel-img-1.webp";
 import Image2 from "../assets/home/carousel-img-2.webp";
 import Image3 from "../assets/home/carousel-img-3.webp";
+import { FaArrowRight } from "react-icons/fa"; // Import arrow icon
 import "./Carousel.css";
 
 const Carousel = () => {
   return (
     <div className="image-grid debug-border">
-      <img src={Image1} alt="Carousel Image 1" className="carousel-image debug-border" />
-      <img src={Image2} alt="Carousel Image 2" className="carousel-image debug-border" />
-      <img src={Image3} alt="Carousel Image 3" className="carousel-image debug-border" />
+      <div className="carousel-item debug-border">
+        <img src={Image1} alt="Carousel Image 1" className="carousel-image debug-border" />
+      </div>
+      <FaArrowRight className="arrow-icon debug-border" />
+      <div className="carousel-item debug-border">
+        <img src={Image2} alt="Carousel Image 2" className="carousel-image debug-border" />
+      </div>
+      <FaArrowRight className="arrow-icon debug-border" />
+      <div className="carousel-item debug-border">
+        <img src={Image3} alt="Carousel Image 3" className="carousel-image debug-border" />
+      </div>
     </div>
   );
 };
