e7040a845380e13e981c06ff5c4d11d52b9d8bb3
sn1tch1 <65768436+sn1tch1@users.noreply.github.com>
2024-09-11 10:55:54 +0300

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -30,23 +30,17 @@ const Footer = () => {
             </Link>
           </li>
           <li className="cursor-pointer text-white/70 hover:text-white duration-300">
-            <a
-              href="URL_FOR_COOKIE_POLICY"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Politica Cookie
-            </a>
-          </li>
+            <li className="cursor-pointer text-white/70 hover:text-white duration-300">
+  <Link to="/cookie-policy">
+    Politica Cookie
+  </Link>
+</li>
           <li className="cursor-pointer text-white/70 hover:text-white duration-300">
-            <a
-              href="URL_FOR_PRIVACY_POLICY"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Politica de Conidentialitate
-            </a>
-          </li>
+            <li className="cursor-pointer text-white/70 hover:text-white duration-300">
+  <Link to="/privacy-policy">
+    Politica de Confidentialitate
+  </Link>
+</li>
         </ul>
       </div>
 
