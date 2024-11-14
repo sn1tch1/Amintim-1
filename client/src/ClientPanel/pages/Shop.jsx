e3dd73708d22ddd8f06a3d743a3951e3925e4d64
Pînzariu Marian <37148227+mpinzariu@users.noreply.github.com>
2024-11-14 19:19:35 +0200

    update values

--- client/src/ClientPanel/pages/Shop.jsx
+++ client/src/ClientPanel/pages/Shop.jsx
@@ -33,8 +33,7 @@ const Shop = () => {
       id: selectedOption,
       image: selectedImage,
       title: "Pachet Pagina Memoriala",
-      // price: selectedOption === "buy1" ? 190 : 280,
-      price: selectedOption === "buy1" ? 19 : 28,
+      price: selectedOption === "buy1" ? 190 : 280,
     };
     addToCart(item);
     toast.success("Added to Cart");
