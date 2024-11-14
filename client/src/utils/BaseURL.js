a6b597a49b4b103e02442e26143d3b57efe00a8b
Pînzariu Marian <37148227+mpinzariu@users.noreply.github.com>
2024-11-14 19:21:30 +0200

    update api

--- client/src/utils/BaseURL.js
+++ client/src/utils/BaseURL.js
@@ -2,7 +2,7 @@
 // const BASE_URL =
 //   "https://amintim-2fn139w3c-uzairs-projects-328814ec.vercel.app/api";
 const BASE_URL = "https://amintim-api.vercel.app/api";
-export const IMAGES_BASE_URL = "http://localhost:5001";
+export const IMAGES_BASE_URL = "http://localhost:5000";
 // export const IMAGES_BASE_URL = "https://amintim-api.vercel.app";
 
 export const PAYMENT_ENV = "sandbox"; //prod
