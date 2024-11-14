c73c9c80d04c7a6d1b459032892476ad394f8206
Pînzariu Marian <37148227+mpinzariu@users.noreply.github.com>
2024-11-14 19:08:23 +0200

    set api to 5000

--- api/index.js
+++ api/index.js
@@ -266,7 +266,7 @@ app.use("/api/purchase", purchaseRoutes);
 app.use("/api/memorial", memorialRoutes);
 app.use("/api/tributes", tributeRoutes);
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(process.env.MONGO_URI || "mongodb://localhost:27017/amintim")
