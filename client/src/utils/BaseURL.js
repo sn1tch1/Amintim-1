db397f078b473de233e617788a3446c681d4adf6
uzair-shakeel <uzairshakeel00000@gmail.com>
2024-11-14 13:23:07 +0500

    base_URL changed

--- client/src/utils/BaseURL.js
+++ client/src/utils/BaseURL.js
@@ -1,7 +1,8 @@
 // const BASE_URL = "http://localhost:5000/api";
 // const BASE_URL =
 //   "https://amintim-2fn139w3c-uzairs-projects-328814ec.vercel.app/api";
-const BASE_URL = "https://amintim-api.vercel.app/api";
+// const BASE_URL = "https://amintim-api.vercel.app/api";
+const BASE_URL = "https://amintim-1-k4xy.vercel.app/api";
 export const IMAGES_BASE_URL = "http://localhost:5000";
 // export const IMAGES_BASE_URL = "https://amintim-api.vercel.app";
 export default BASE_URL;
