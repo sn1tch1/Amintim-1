c3b2469d100f35225fe213d5153503684a915e07
sn1tch1 <65768436+sn1tch1@users.noreply.github.com>
2024-11-28 16:38:53 +0200

    Update sendEmail.js
    translate purchase email

--- api/utils/sendEmail.js
+++ api/utils/sendEmail.js
@@ -17,11 +17,11 @@ const sendEmail = async (to, subject, keysFlat) => {
   const htmlContent = `
     <div style="font-family: Arial, sans-serif; color: #333; padding: 20px; max-width: 600px; margin: auto; border: 1px solid #ddd; border-radius: 8px;">
       <h1 style="color: #007BFF;">Amintim</h1>
-      <p style="font-size: 16px; line-height: 1.5;">Hello,</p>
-      <p style="font-size: 16px; line-height: 1.5;">Thank you for your purchase from Amintim! We're excited to let you know that you have purchased a Soul Star.</p>
-      <p style="font-size: 16px; line-height: 1.5;">If you have any questions or need assistance, feel free to <a href="mailto:support@amintim.com" style="color: #007BFF;">contact our support team</a>.</p>
-      <p style="font-size: 16px; line-height: 1.5;">Best regards,</p>
-      <p style="font-size: 16px; line-height: 1.5;">The Amintim Team</p>
+      <p style="font-size: 16px; line-height: 1.5;">Salutare!</p>
+      <p style="font-size: 16px; line-height: 1.5;">Îți mulțumim pentru achiziția ta de la Amintim. Suntem încântați să te anunțăm că ai cumpărat un Profil Memorial QR - un mod minunat de a onora amintirea celor dragi.</p> 
+      <p style="font-size: 16px; line-height: 1.5;">Dacă ai întrebări sau ai nevoie de asistență, nu ezita să <a href="mailto:contact@amintim.ro" style="color: #007BFF;">contactezi echipa noastră</a>. Suntem aici să te ajutăm!</p>
+      <p style="font-size: 16px; line-height: 1.5;">Toate cele bune,</p>
+      <p style="font-size: 16px; line-height: 1.5;">Echipa Amintim</p>
     </div>
   `;
 
