5a11d0f9b9540d8a0f3684876138d077ba06f126
sn1tch1 <65768436+sn1tch1@users.noreply.github.com>
2024-11-28 16:33:14 +0200

    Update sendVerification.js
    translated welcome email

--- api/utils/sendVerification.js
+++ api/utils/sendVerification.js
@@ -15,13 +15,13 @@ const sendEmail = async (to, subject, verificationCode) => {
   const htmlContent = `
     <div style="font-family: Arial, sans-serif; color: #333; padding: 20px; max-width: 600px; margin: auto; border: 1px solid #ddd; border-radius: 8px;">
       <h1 style="color: #007BFF;">Amintim</h1>
-      <p style="font-size: 16px; line-height: 1.5;">Hello,</p>
-      <p style="font-size: 16px; line-height: 1.5;">Thank you for your Signing up in Amintim! We're excited to welcome you.</p>
-      <p style="font-size: 16px; line-height: 1.5;"><strong>Verify Your Email</strong></p>
-      <p style="font-size: 16px; line-height: 1.5;"><strong>Your Verification Code is:</strong> <code style="background: #f1f1f1; padding: 4px; border-radius: 4px;">${verificationCode}</code></p>
-      <p style="font-size: 16px; line-height: 1.5;">If you have any questions or need assistance, feel free to <a href="mailto:support@amintim.com" style="color: #007BFF;">contact our support team</a>.</p>
-      <p style="font-size: 16px; line-height: 1.5;">Best regards,</p>
-      <p style="font-size: 16px; line-height: 1.5;">The Amintim Team</p>
+      <p style="font-size: 16px; line-height: 1.5;">Salutare,</p>
+      <p style="font-size: 16px; line-height: 1.5;">Îți mulțumim pentru înregistrarea pe Amintim! Suntem onorați să te avem alături.</p>
+      <p style="font-size: 16px; line-height: 1.5;"><strong>Verifică-ți Adresa de Email</strong></p>
+      <p style="font-size: 16px; line-height: 1.5;"><strong>Codul Tău de Verificare este:</strong> <code style="background: #f1f1f1; padding: 4px; border-radius: 4px;">${verificationCode}</code></p>
+      <p style="font-size: 16px; line-height: 1.5;">Dacă ai întrebări sau ai nevoie de ajutor, nu ezita să <a href="mailto:contact@amintim.ro" style="color: #007BFF;">contactezi echipa noastră de suport</a>.</p>
+      <p style="font-size: 16px; line-height: 1.5;">Cele mai bune urări,</p>
+      <p style="font-size: 16px; line-height: 1.5;">Echipa Amintim</p>
     </div>
   `;
 
