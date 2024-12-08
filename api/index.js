e42a5946f4005883bfc7951cf2e7ebde169d2029
Uzair Shakeel <144268023+uzair-shakeel@users.noreply.github.com>
2024-12-08 23:12:29 +0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const cors = require("cors");
+const cors = require("cors");   
 const userRoutes = require("./routes/userRoutes.js");
 const purchaseRoutes = require("./routes/purchaseRoutes.js");
 const { uploadProfileImage } = require("./controllers/userController.js");
